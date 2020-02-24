7c6796d6484f943ce2e1669b36864bb862d14e18
Rayhax96 <alex_1mrv@hotmail.com>
2020-02-23 20:25:28 -0600

    BESA

--- src/app/cuadromenu/principal/principal.component.ts
+++ src/app/cuadromenu/principal/principal.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./estilo.css']
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit(): void {
   }
